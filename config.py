878158e9d45ad1609a309663c341ec303e6146c4
fordgoat <adaadaajapan@gmail.com>
2025-01-25 01:40:36 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
-MONGO_DB = getenv("MONGO_DB", "")
-LOG_GROUP = getenv("LOG_GROUP", "")
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
+API_ID = int(getenv("API_ID", "29486311"))
+API_HASH = getenv("API_HASH", "ffdc688dc4eee8d2585cb24155188432")
+BOT_TOKEN = getenv("BOT_TOKEN", "7300347870:AAFNfnj1sEUQNnE1zdyFq-xgY8UkeuKjubw")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "1707380693").split()))
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://ucik:ucik@cluster0.0l3r8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+LOG_GROUP = getenv("LOG_GROUP", "-1002339772659")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002234563714"))
